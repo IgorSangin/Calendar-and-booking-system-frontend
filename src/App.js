ab6b2297deed9ff95c6a8cb6968aed3c9ebc1a9a
Carl Egonu <carlegonu@macbook-pro.home>
2019-10-31 14:26:09 +0000

    getting_closer

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import Comment from './components/CommentList';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import { Button } from 'antd';
+import CommentList from './components/CommentList';
 
 function App() {
+
+  let comments =[
+    {
+      "id":1,
+      "name": "Carl",
+      "URL": "https://i.pinimg.com/originals/37/d7/84/37d78445625a6c74b37ff9df36144ce0.jpg",
+      "alt": "Carl",
+      "content": "it's coming together"
+    }
+  ];
+
   return (
     <div style={{ background: '#ECECEC', padding: '30px' }}>
     <Signup/>
-    <Comment/>
+    <CommentList comments={comments}/>
     </div>
     )
 }
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,13 +2,25 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import moment from 'moment'
 import Comment from './Comments'
-import { Col, Row } from 'antd';
+import { Col, Row, Avatar } from 'antd';
 
 class CommentList extends React.Component{
+
     render(){
+        let list = [];
+        let i =0;
         return(
-        <li> <Comment name ="Carl"/> </li>
-        )
+        function printComments(length,element){
+            for(i=0; i<length; i++){
+            list+= <Comment key={element.id} name={element.name} avatar={ <Avatar URL={element.URL} alt={element.name}/>}
+            content={element.content}/>
+            }
+            return list;
+        })
+        // return(
+        // <li> <Comment name ="Carl"
+        // URL="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg"/> </li>
+        // )
     }
     // oneRow(comments, rowNumber){
 
