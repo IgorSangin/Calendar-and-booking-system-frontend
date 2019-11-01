0e0a190771f2b8772ca2e4baf53faefe478cd6b3
Igor Sangin <sangini@coventry.ac.uk>
2019-11-01 17:46:48 +0000

    Working on login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,20 +4,97 @@ import 'antd/dist/antd.css';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
 class LoginForm extends React.Component {
+
+  state = {
+    confirmDirty: false,
+    addedSucessfully: false, //if the user is added successfully
+    showSuccess: false, //if should we show a successful feedback message after adding a user
+    showError: false, //if should we show an error feedback message after adding a user
+    errorCode: 400,  //to save the errorCode we recieved from the api server
+    responseStatus: "nothing",  //the validation status of the email
+    errorMessage: "",   //the error message to display to the user after server rejects action
+  };
+
   handleSubmit = e => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+    this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
+        //echo the values to the browser console to make sure they are correct
         console.log('Received values of form: ', values);
+
+        //here we should send a request to our server to post the user
+
+        //use fetch API to post the user data
+        fetch('http://localhost:3000/api/login', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({values})
+        }).then(res => {
+          if(res.ok)
+            this.setState({addedSucessfully:true})
+          else
+            this.setState({
+              addedSucessfully:false,
+              errorCode: res.status
+            });
+
+            return res.json()
+        }).then(data => this.checkResponse(data))
       }
     });
   };
+
+
+  handleConfirmBlur = e => {
+    const { value } = e.target;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+
+  compareToFirstPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
+  };
+
+  validateToNextPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  };
+
+  checkResponse = (data) => {
+
+    if(this.state.addedSucessfully){
+      this.props.form.resetFields();
+      this.setState({
+        showSuccess:true,
+        showError : false
+      });
+    }
+    else{
+      //handle errors
+      this.setState({
+        errorMessage: data.message,
+        showSuccess:false,
+        showError : true, 
+        responseStatus: "error"
+      });
+    }
+  }
 
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item>
+        <Form.Item label="Username">
           {getFieldDecorator('username', {
             rules: [{ required: true, message: 'Please input your username!' }],
           })(
@@ -27,9 +104,15 @@ class LoginForm extends React.Component {
             />,
           )}
         </Form.Item>
-        <Form.Item>
+        <Form.Item label="Password">
           {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
+            rules: [
+              {
+                required: true,
+                message: 'Please input your Password!'
+              },
+            ],
+
           })(
             <Input
               prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
