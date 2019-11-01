c8f59a5b0554a8bed87840d62b2b55b3e8f928bb
Carl Egonu <carlegonu@MacBook-Pro.local>
2019-11-01 15:13:36 +0000

    Comment_UI_0.1

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -6,48 +6,9 @@ import { Col, Row, Avatar } from 'antd';
 
 class CommentList extends React.Component{
 
-    render(){
-        let list = [];
-        let i =0;
-        return(
-        function printComments(length,element){
-            for(i=0; i<length; i++){
-            list+= <Comment key={element.id} name={element.name} avatar={ <Avatar URL={element.URL} alt={element.name}/>}
-            content={element.content}/>
-            }
-            return list;
-        })
-        // return(
-        // <li> <Comment name ="Carl"
-        // URL="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg"/> </li>
-        // )
+    printComments(comments){
+        
     }
-    // oneRow(comments, rowNumber){
-
-    //     let row = comment.map(element =>{
-
-    //         return<>
-    //             <Col>
-    //                 {element !== null ?(
-    //                     <Comment key={element.id} name={element.name} 
-    //                     text={element.text} avatar={element.avatar}></Comment>
-    //                 ) : null}
-    //             </Col>
-    //         </>
-
-    //     });
-
-    //     return<div key={rowNumber}>
-    //     <Row type ="flex" justify="center">
-    //         {row}
-    //     </Row>
-    //     <br/>
-    //     </div>
-    // }
-
-    // render(){
-
-
-    // }
+    
 }
 export default CommentList;
