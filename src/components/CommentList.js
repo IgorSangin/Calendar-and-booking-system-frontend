e19774f434c8e99292861a75ce31de238eb90645
Carl Egonu <carlegonu@MacBook-Pro.local>
2019-11-01 16:01:48 +0000

    CommentUI_0.2

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,11 +3,33 @@ import 'antd/dist/antd.css';
 import moment from 'moment'
 import Comment from './Comments'
 import { Col, Row, Avatar } from 'antd';
+import Comments from './Comments';
 
 class CommentList extends React.Component{
 
     printComments(comments){
+
+        let comment = comments.map(element =>{
+            
+            return<>
+                {element != null ?(
+                    <Comment key={element.id} author={element.name} 
+                    avatar={<Avatar src={element.URL} alt={element.name}/>} 
+                    content={element.text}/>):null}
+            </>
+        })
+
         
+    }
+
+    render(){
+        return <div>
+            <li>
+                <ul>
+                    {this.printComments()}
+                </ul>
+            </li>
+        </div>
     }
     
 }
