569ce322daf27d0361e63d3c13a35742d01db1be
Igor Sangin <sangini@coventry.ac.uk>
2019-12-06 13:20:28 +0000

    Working on drag and drop

--- src/components/calendar/MyCalendar.js
+++ src/components/calendar/MyCalendar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Col, Row } from "antd";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
 
 import "./styles.css";
 
@@ -14,21 +15,64 @@ import "@fullcalendar/timegrid/main.css";
 class MyCalendar extends React.Component {
 
   state = {
-    weekends: true,
-    events: [
-      // default event
-      { title: "Default event", start: new Date() }
+    activities: [
+      { title: "Go shopping", id: "1" },
+      { title: "Play games", id: "2" },
+      { title: "Walk the dog", id: "3" },
+      { title: "Do cleaning", id: "4" },
     ]
   };
+
+  // Making the activities draggable
+  componentDidMount() {
+    let element = document.getElementById("activities-box");
+    new Draggable(element, {
+      itemSelector: ".fc-event",
+      eventData: function(activityEl) {
+        let title = activityEl.getAttribute("title");
+        let id = activityEl.getAttribute("data");
+        return {
+          title: title,
+          id: id
+        };
+      }
+    });
+  }
 
   render() {
     return (
       <div className="page">
         <div className="page-top">
-          <button onClick={this.toggleWeekends}>Toggle weekends</button>&nbsp;
+        <Row>
+          <Col lg={3} sm={3} md={3}>
+            <div
+              id="activities-box"
+              style={{
+                padding: "10px",
+                width: "80%",
+                height: "auto",
+                maxHeight: "-webkit-fill-available"
+              }}
+            >
+              <p align="center">
+                <strong>Activities</strong>
+              </p>
+              {this.state.activities.map(activity => (
+                <div
+                  className="fc-event"
+                  title={activity.title}
+                  data={activity.id}
+                  key={activity.id}
+                >
+                  {activity.title}
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
         </div>
         <div className="calendar">
-        <h1 align="center" style={{color: "red"}}>Click on a date to add an event</h1>
+        <h1 align="center" style={{color: "red"}}>Click on a date to add an activity</h1>
           <FullCalendar
             defaultView="dayGridMonth"
             header={{
@@ -37,8 +81,7 @@ class MyCalendar extends React.Component {
               right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
             }}
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            weekends={this.state.weekends}
-            events={this.state.events}
+            activities={this.state.activities}
             dateClick={this.handleDateClick}
           />
         </div>
@@ -46,21 +89,14 @@ class MyCalendar extends React.Component {
     );
   }
 
-  toggleWeekends = () => {
-    this.setState({
-      // update a property
-      weekends: !this.state.weekends
-    });
-  };
-
 
   handleDateClick = arg => {
-    if (window.confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+    if (window.confirm("Would you like to add an activity to " + arg.dateStr + " ?")) {
       this.setState({
-        // add new event data
-        events: this.state.events.concat({
+        // add new activity data
+        activities: this.state.activities.concat({
           // creates a new array
-          title: "New Event",
+          title: "My activity",
           start: arg.date,
           allDay: arg.allDay
         })
